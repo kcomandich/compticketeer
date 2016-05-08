e8fbe28ba3640615c29a2217865826d776fe9555
Kirsten Comandich <kcomandich@gmail.com>
2016-05-07 18:19:39 -0700

    Environment variable EVENTBRITE_OAUTH_TOKEN was spelled wrong - EVENTBRITE_OATH_TOKEN.

--- config/initializers/eventbrite.rb
+++ config/initializers/eventbrite.rb
@@ -2,7 +2,7 @@
 Rails.application.configure do
   config.eventbrite = {
     url: 'https://www.eventbriteapi.com/v3',
-    oauth_token: ENV['EVENTBRITE_OATH_TOKEN'],
+    oauth_token: ENV['EVENTBRITE_OAUTH_TOKEN'],
     event_id: ENV['EVENTBRITE_EVENT_ID']
   }
 end
